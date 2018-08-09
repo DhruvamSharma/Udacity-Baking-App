f8453c7440c4e1bd667cb9c432037fc5f4baaac4
Dhruvam <dhruvamssharma@gmail.com>
2018-08-10 00:17:11 +0530

    developer commit

--- app/src/main/java/com/udafil/dhruvamsharma/bakingandroidapp/main/MainActivity.java
+++ app/src/main/java/com/udafil/dhruvamsharma/bakingandroidapp/main/MainActivity.java
@@ -17,6 +17,8 @@ import com.udafil.dhruvamsharma.bakingandroidapp.R;
 import com.udafil.dhruvamsharma.bakingandroidapp.data.model.RecipeModel;
 import com.udafil.dhruvamsharma.bakingandroidapp.databinding.ActivityMainBinding;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private MainActivityViewModel viewModel;
@@ -62,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     private void setupActivity() {
 
         //Getting data from the view model
-        RecipeModel[] recipeData = viewModel.getRecipeData();
+        List<RecipeModel> recipeData = viewModel.getRecipeData();
 
         //Setting up the recycler view
         mAdapter = new RecipeListAdapter(recipeData, this);

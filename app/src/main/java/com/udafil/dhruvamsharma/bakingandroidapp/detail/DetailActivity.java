42c760a5bc15b49383f70dbd221edd948e118fa3
Dhruvam <dhruvamssharma@gmail.com>
2018-08-21 14:22:37 +0530

    work started

--- app/src/main/java/com/udafil/dhruvamsharma/bakingandroidapp/detail/DetailActivity.java
+++ app/src/main/java/com/udafil/dhruvamsharma/bakingandroidapp/detail/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.udafil.dhruvamsharma.bakingandroidapp.detail;
 
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +11,14 @@ import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.udafil.dhruvamsharma.bakingandroidapp.R;
+import com.udafil.dhruvamsharma.bakingandroidapp.data.model.RecipeModel;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -22,6 +28,8 @@ public class DetailActivity extends AppCompatActivity {
     private static int windowIndex = 0;
     private static long playBackPosition = 0;
     private static boolean playWhenReady = true;
+
+    private RecipeModel recipeModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +54,34 @@ public class DetailActivity extends AppCompatActivity {
         mExoPlayer.setPlayWhenReady(playWhenReady);
         mExoPlayer.seekTo(windowIndex,playBackPosition);
 
+
+        Uri uri = Uri.parse(recipeModel.getSteps().get(0).getVideoURL());
+        MediaSource mediaSource = buildMediaSource(uri);
+        mExoPlayer.prepare(mediaSource);
+
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(
+                new DefaultHttpDataSourceFactory("BakingAndroidApp")).
+                createMediaSource(uri);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || mExoPlayer == null)) {
+            initializePlayer();
+        }
     }
 
 

60abc23da52273751ef3f375f80e6ee4736f39d8
Dhruvam <dhruvamssharma@gmail.com>
2019-02-20 20:25:41 +0530

    new design rough layout added

--- app/src/main/java/com/udafil/dhruvamsharma/bakingandroidapp/main/MainActivity.java
+++ app/src/main/java/com/udafil/dhruvamsharma/bakingandroidapp/main/MainActivity.java
@@ -1,13 +1,8 @@
 package com.udafil.dhruvamsharma.bakingandroidapp.main;
 
-import android.arch.lifecycle.ViewModel;
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
-import android.databinding.DataBindingUtil;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.annotation.VisibleForTesting;
 import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
@@ -15,10 +10,8 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.transition.Slide;
 import android.widget.Toast;
 
-import com.squareup.leakcanary.LeakCanary;
 import com.udafil.dhruvamsharma.bakingandroidapp.R;
 import com.udafil.dhruvamsharma.bakingandroidapp.data.model.RecipeModel;
 import com.udafil.dhruvamsharma.bakingandroidapp.main.IdlingResource.RecipeIdlingResource;
@@ -65,50 +58,28 @@ public class MainActivity extends AppCompatActivity implements MessageDelayer.De
      * MainActivity setup method that displays data on the views
      */
     private void setupActivity() {
-
         //testing purpose only
         MessageDelayer.processMessage("hi", this, mIdlingResource, this);
-
         mainRecipeListRv = findViewById(R.id.main_recipe_list_rv);
-
         //Getting data from the view model
         List<RecipeModel> recipeData = viewModel.getRecipeData();
-
         //Setting up the recycler view
         mAdapter = new RecipeListAdapter(recipeData, this);
-
         mainRecipeListRv.setAdapter(mAdapter);
-
         if(getResources().getBoolean(R.bool.isTablet)) {
-
             GridLayoutManager manager = new GridLayoutManager(this, 2);
-            //manager.setOrientation(LinearLayoutManager.VERTICAL);
-
+            manager.setOrientation(LinearLayoutManager.VERTICAL);
             mainRecipeListRv.setLayoutManager(manager);
-
         } else {
-
             LinearLayoutManager manager = new LinearLayoutManager(this);
             manager.setOrientation(LinearLayoutManager.VERTICAL);
-
             //mainRecipeListRv.setBackgroundResource(R.drawable.ic_main_activity_background);
-
             mainRecipeListRv.setLayoutManager(manager);
-
         }
-
-
-
     }
-
-
-
-
     @Override
     public void onDone(List<RecipeModel> recipeModel) {
-
         Toast.makeText(this, "done",Toast.LENGTH_SHORT).show();
-
     }
 
     /**
